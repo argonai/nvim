54cc3adb6c2015e05fe0e069d0d4dd7933670162
Aaron Vandeweghe <aaronvdw@protonmail.com>
2025-01-08 22:22:32 +0100

    start new config

---
+++ lua/custom/plugins/misc.lua
@@ -0,0 +1,17 @@
+return {
+  {
+    'folke/zen-mode.nvim',
+    opts = {
+      -- your configuration comes here
+      -- or leave it empty to use the default settings
+      -- refer to the configuration section below
+      vim.keymap.set('n', '<leader>tz', ':ZenMode', { desc = '[T]oggle [Z]en mode' }),
+    },
+  },
+  {
+    'akinsho/toggleterm.nvim',
+    version = '*',
+    config = true,
+    vim.keymap.set('n', '<leader>tt', ':ToggleTerm', { desc = '[T]oggle [T]erm' }),
+  },
+}
---
+++ lua/kickstart/plugins/neo-tree.lua
@@ -0,0 +1,33 @@
+-- Neo-tree is a Neovim plugin to browse the file system
+-- https://github.com/nvim-neo-tree/neo-tree.nvim
+
+return {
+  'nvim-neo-tree/neo-tree.nvim',
+  version = '*',
+  dependencies = {
+    'nvim-lua/plenary.nvim',
+    'nvim-tree/nvim-web-devicons', -- not strictly required, but recommended
+    'MunifTanjim/nui.nvim',
+  },
+  cmd = 'Neotree',
+  keys = {
+    { '<leader>e', ':Neotree reveal<CR>', desc = 'NeoTree reveal', silent = true },
+  },
+  opts = {
+    filesystem = {
+      filtered_items = {
+        always_show = { -- remains visible even if other settings would normally hide it
+          '.gitignore',
+        },
+        always_show_by_pattern = { -- uses glob style patterns
+          '.env*',
+        },
+      },
+      window = {
+        mappings = {
+          ['\\'] = 'close_window',
+        },
+      },
+    },
+  },
+}
