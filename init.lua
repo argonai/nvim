7adbb7502487c3e251a23e27234a0cce91311147
Aaron Vandeweghe <aaronvdw@protonmail.com>
2025-01-09 23:28:52 +0100

    add go and enable some kickstart plugins

--- init.lua
+++ init.lua
@@ -145,9 +145,9 @@ require('lazy').setup({
   'tpope/vim-sleuth', -- Detect tabstop and shiftwidth automatically
   --
   -- require 'kickstart.plugins.debug',
-  -- require 'kickstart.plugins.indent_line',
-  -- require 'kickstart.plugins.lint',
-  -- require 'kickstart.plugins.autopairs',
+  require 'kickstart.plugins.indent_line',
+  require 'kickstart.plugins.lint',
+  require 'kickstart.plugins.autopairs',
   require 'kickstart.plugins.neo-tree',
   -- require 'kickstart.plugins.gitsigns', -- adds gitsigns recommend keymaps
 
