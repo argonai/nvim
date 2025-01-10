0e3de90bf999e06a54b6ee2fa976e21143ae2fac
Aaron Vandeweghe <aaronvdw@protonmail.com>
2025-01-10 01:16:41 +0100

    add harpoon

--- init.lua
+++ init.lua
@@ -182,5 +182,66 @@ require('lazy').setup({
     },
   },
 })
+local harpoon = require 'harpoon'
+harpoon:setup()
+
+local conf = require('telescope.config').values
+local function toggle_telescope(harpoon_files)
+  local make_finder = function()
+    local paths = {}
+    for _, item in ipairs(harpoon_files.items) do
+      table.insert(paths, item.value)
+    end
+
+    return require('telescope.finders').new_table {
+      results = paths,
+    }
+  end
+
+  require('telescope.pickers')
+    .new({}, {
+      prompt_title = 'Harpoon',
+      finder = make_finder(),
+      previewer = conf.file_previewer {},
+      sorter = conf.generic_sorter {},
+      attach_mappings = function(prompt_buffer_number)
+        vim.keymap.set(
+          'i',
+          '<A-d>', -- your mapping here
+          function()
+            local state = require 'telescope.actions.state'
+            local selected_entry = state.get_selected_entry()
+            local current_picker = state.get_current_picker(prompt_buffer_number)
+            harpoon:list():remove_at(selected_entry.index)
+            current_picker:refresh(make_finder())
+          end
+        )
+
+        return true
+      end,
+    })
+    :find()
+end
+
+vim.keymap.set('n', '<leader>ha', function()
+  harpoon:list():add()
+end, { desc = '[H]arpoon [A]dd' })
+vim.keymap.set('n', '<leader>hl', function()
+  toggle_telescope(harpoon:list())
+end, { desc = '[H]arpoon [L]ist' })
+
+vim.keymap.set('n', '<leader>h1', function()
+  harpoon:list():select(1)
+end, { desc = '[H]arpoon [1]' })
+vim.keymap.set('n', '<leader>h2', function()
+  harpoon:list():select(2)
+end, { desc = '[H]arpoon [2]' })
+vim.keymap.set('n', '<leader>h3', function()
+  harpoon:list():select(3)
+end, { desc = '[H]arpoon [3]' })
+vim.keymap.set('n', '<leader>h4', function()
+  harpoon:list():select(4)
+end, { desc = '[H]arpoon [4]' })
+
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
